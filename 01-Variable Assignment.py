9cc285895d3b3eff9e6c1dcbf18b65f6eb03f504
Anup Bhattarai <anupratap12@gmail.com>
2019-01-05 06:06:49 +1100

    task one string

---
+++ 01-Variable Assignment.py
@@ -0,0 +1,10 @@
+a = 30
+a += 10
+a
+#print(a)
+
+b = (4,6,10)
+print(b)
+
+## prints type of a varibale 
+print(type(b))
---
+++ basicDataStructure.py
@@ -0,0 +1,9 @@
+# normal division
+a = 5/2
+print(a)
+
+## floor division
+b = 7//2
+print(b)
+
+
