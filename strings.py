09b660ff224494e771a2de32696d8d6220731231
Anup Bhattarai <anupratap12@gmail.com>
2019-01-05 06:56:01 +1100

    working on python strings

---
+++ strings.py
@@ -0,0 +1,10 @@
+## single ' and double "" quotes can be used to create strings
+# Better to use double quote to make use of sigle quote in between somewhere
+
+#\n newline character can be used to print in different lines.
+
+print('\n G - Grocery \n T - Travel \n P - Payment \n PC - PersonalCare \n E - Eating Out \n S - Shopping \n ED - Education \n R - Refund \n')
+
+p = 'Hello worlds'
+length = len(p)
+print(length)
