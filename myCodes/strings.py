adbe85cb4fc9ef543745807f7ea1155f8453b31d
Anup Bhattarai <anupratap12@gmail.com>
2021-03-15 08:55:47 +1100

    Strings Done

--- myCodes/strings.py
+++ myCodes/strings.py
@@ -21,3 +21,30 @@ print(s[:-1])  # print everything but the last letter
 print(s[::1])  # grab everything at a step size of 1
 print(s[::2])  # Everything in a step size of 2
 print(s[::-1])  # print string reverse/backwards
+
+
+# String PROPERTIES
+# STRINGS ARE IMMUTABLE
+
+################
+#   s[0] = 'x'
+##################
+
+# String concatenation and reassiging to a new variable is possible
+
+print(s+'concatenated strings')
+s = s+'further concatenation and assigning to same variable is possible so if need to change anything in a string it is better to re assign BUT STILL CANT CHANGE WHAT WAS ON S'
+
+print(s)
+
+# BUILT IN STRING METHODS
+print(s.upper())
+print(s.lower())
+print(s.split())  # GIVES LIST OF SLPITTED WORDS
+# splititng by an element, but doesnt take the split location element with it
+print('\n\n\n')
+print(s.split('e'))
+
+
+# STRING FORMATTING
+print('pring another string with curly brackets: {}'.format('the inserted string'))
